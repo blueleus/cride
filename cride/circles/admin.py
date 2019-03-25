7895c130f56f98c96e58c8a8d098c17b14b8d9d3
Andres Orejuela <andresforejuelab@gmail.com>
2019-03-24 22:51:48 -0500

    Modelo Circle y crear circles por medio de archivo csv.

---
+++ cride/circles/admin.py
@@ -0,0 +1,62 @@
+"""Circle models admin."""
+
+# Django
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
+
+# Models
+from cride.circles.models import Circle
+
+# Forms
+from .forms import UploadFileForm
+
+# Utilities
+import csv
+from io import TextIOWrapper
+
+
+@admin.register(Circle)
+class CircleAdmin(admin.ModelAdmin):
+    """Circle model admin."""
+
+    list_display = (
+        'slug_name',
+        'name',
+        'is_public',
+        'verified',
+        'is_limited',
+        'members_limit'
+    )
+    search_fields = ('slug_name', 'name')
+    list_filter = (
+        'is_public',
+        'verified',
+        'is_limited'
+    )
+
+    def get_urls(self):
+        urls = super(CircleAdmin, self).get_urls()
+        custom_urls = [
+            path('import-csv/', self.import_csv)
+        ]
+        return custom_urls + urls
+
+    def import_csv(self, request):
+        if request.method == 'POST':
+            form = UploadFileForm(request.POST, request.FILES)
+            if form.is_valid():
+                self.handle_import_csv(request.FILES['file'])
+                self.message_user(request, "Your csv file has been imported")
+        else:
+            form = UploadFileForm()
+        return render(request, 'admin/circles/circle/csv_form.html', {'form': form})
+
+    def handle_import_csv(self, f):
+        # For more information:
+        # https://stackoverflow.com/questions/16243023/how-to-resolve-iterator-should-return-strings-not-bytes
+        file = TextIOWrapper(f.file, encoding='utf-8')
+        reader = csv.DictReader(file)
+        for row in reader:
+            circle = Circle(**row)
+            circle.save()
---
+++ cride/circles/apps.py
@@ -0,0 +1,11 @@
+"""Circle app."""
+
+# Django
+from django.apps import AppConfig
+
+
+class CircleAppConfig(AppConfig):
+    """Circle app config."""
+
+    name = 'cride.circles'
+    verbose_name = 'Circle'
