61569eed34213dcac01a2eceddc98903b2aea6d2
Andres Orejuela <andresforejuelab@gmail.com>
2019-04-01 20:45:10 -0500

    Autenticación por token, ViewSets y Paginación.

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -3,10 +3,14 @@
 # Django
 from django.urls import include, path
 
+from rest_framework.routers import DefaultRouter
+
 # Views
-from cride.circles.views import list_circles, create_circles
+from cride.circles.views import CircleViewSet
+
+router = DefaultRouter()
+router.register(r'circles', CircleViewSet, basename='circle')
 
 urlpatterns = [
-    path('circles/', list_circles),
-    path('circles/create/', create_circles)
+    path('', include(router.urls))
 ]
