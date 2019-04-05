9d20e7de00b33d821110012942ad79eae7267131
Andres Orejuela <andresforejuelab@gmail.com>
2019-04-04 22:35:04 -0500

    Circle members.

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -7,9 +7,15 @@ from rest_framework.routers import DefaultRouter
 
 # Views
 from cride.circles.views import CircleViewSet
+from .views import memberships as membership_views
 
 router = DefaultRouter()
 router.register(r'circles', CircleViewSet, basename='circle')
+router.register(
+    r'circles/(?P<slug_name>[-a-zA-Z0-9_]+)/members',
+    membership_views.MembershipViewSet,
+    basename='membership'
+)
 
 urlpatterns = [
     path('', include(router.urls))
